9e4e650949c356041d76e1efc5387bc77ea91bf6
Anurag Shukla <160250034@ggi.ac.in>
2026-05-18 12:30:43 +0530

    .env remove and baseurl direct use in file

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "",
+    baseURL: import.meta.env.https://moodify-1-hk4g.onrender.com/api || "",
     withCredentials: true
 })
 
