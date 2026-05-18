dab01ec429a699317cde1f6cf9d99c2832fbae50
Anurag Shukla <160250034@ggi.ac.in>
2026-05-18 12:44:33 +0530

    delete env and add base url direct to file

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.https://moodify-1-hk4g.onrender.com/api || "",
+    baseURL: "https://moodify-1-hk4g.onrender.com/api",
     withCredentials: true
 })
 
--- Frontend/src/features/home/service/song.api.js
+++ Frontend/src/features/home/service/song.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "",
+    baseURL: "https://moodify-1-hk4g.onrender.com/api",
     withCredentials: true
 })
 
